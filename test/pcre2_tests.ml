81cc5c10f8e2424263fbb09ab50f750a9fbf4a16
Cooper Pierce <cooper@semgrep.com>
2024-06-21 16:06:52 -0700

    test: correct actual vs expected; addt'l coverage
    Previously we flipped the order for actual and expected values, so
    messages would be printed backwards in the case of a test failure.
    
    This also adds some test coverage for errors due to a bad subject offset.

--- test/pcre2_tests.ml
+++ test/pcre2_tests.ml
@@ -1,50 +1,73 @@
 open OUnit2
 open! Pcre2
 
-let ( >+= ) = Fun.flip Option.map
-let ( >>= ) = Option.bind
+let ( >+= ) x f =
+  match x with
+  | Ok (Some x) -> Ok (Some (f x))
+  | Ok None -> Ok None
+  | Error e -> Error e
+
+let ( >>= ) x f =
+  match x with
+  | Ok (Some x) -> Ok (f x)
+  | Ok None -> Ok None
+  | Error e -> Error e
 
 let simple_test ctxt =
-  match compile "abc" with
-  | Error e -> assert_failure ("failed to compile: " ^ show_compile_error e)
-  | Ok re ->
-      let printer = [%show: range option] in
-      assert_equal ~printer
-        (find re "abc" >+= range_of_match)
-        (Some { start = 0; end_ = 3 });
-      assert_equal ~printer
-        (find re "123abc456" >+= range_of_match)
-        (Some { start = 3; end_ = 6 });
-      assert_equal ~printer
-        (find re "123abc" >+= range_of_match)
-        (Some { start = 3; end_ = 6 });
-      assert_equal ~printer (find re "123ac" >+= range_of_match) None
+  Interp.(
+    match compile "abc" with
+    | Error e -> assert_failure ("failed to compile: " ^ show_compile_error e)
+    | Ok re ->
+        let printer = [%show: (range option, match_error) result] in
+        assert_equal ~printer
+          (Ok (Some { start = 0; end_ = 3 }))
+          (find re "abc" >+= range_of_match);
+        assert_equal ~printer
+          (Ok (Some { start = 3; end_ = 6 }))
+          (find re "123abc456" >+= range_of_match);
+        assert_equal ~printer
+          (Ok (Some { start = 3; end_ = 6 }))
+          (find re "123abc" >+= range_of_match);
+        assert_equal ~printer (Ok None) (find re "123ac" >+= range_of_match))
 
 let simple_captures ctxt =
-  match compile "(a)(b)(c)" with
-  | Error e -> assert_failure ("failed to compile: " ^ show_compile_error e)
-  | Ok re ->
-      let printer = [%show: range option] in
-      let c = captures re "abc" in
-      assert_equal ~printer
-        (c >>= (fun c -> match_of_captures c 0) >+= range_of_match)
-        (Some { start = 0; end_ = 3 });
-      assert_equal ~printer
-        (c >>= (fun c -> match_of_captures c 1) >+= range_of_match)
-        (Some { start = 0; end_ = 1 });
-      assert_equal ~printer
-        (c >>= (fun c -> match_of_captures c 2) >+= range_of_match)
-        (Some { start = 1; end_ = 2 });
-      assert_equal ~printer
-        (c >>= (fun c -> match_of_captures c 3) >+= range_of_match)
-        (Some { start = 2; end_ = 3 })
+  Interp.(
+    match compile "(a)(b)(c)" with
+    | Error e -> assert_failure ("failed to compile: " ^ show_compile_error e)
+    | Ok re ->
+        let printer = [%show: (range option, match_error) result] in
+        let c = captures re "abc" in
+        assert_equal ~printer
+          (Ok (Some { start = 0; end_ = 3 }))
+          (c >>= (fun c -> match_of_captures c 0) >+= range_of_match);
+        assert_equal ~printer
+          (Ok (Some { start = 0; end_ = 1 }))
+          (c >>= (fun c -> match_of_captures c 1) >+= range_of_match);
+        assert_equal ~printer
+          (Ok (Some { start = 1; end_ = 2 }))
+          (c >>= (fun c -> match_of_captures c 2) >+= range_of_match);
+        assert_equal ~printer
+          (Ok (Some { start = 2; end_ = 3 }))
+          (c >>= (fun c -> match_of_captures c 3) >+= range_of_match))
 
 let bad_pattern ctxt =
-  match compile "ab(" with
-  | Error MISSING_CLOSING_PARENTHESIS -> ()
-  | Error e ->
-      assert_failure ("Incorrectly error for pattern: " ^ show_compile_error e)
-  | Ok _ -> assert_failure "Incorrectly compiled invalid pattern"
+  Interp.(
+    match compile "ab(" with
+    | Error MISSING_CLOSING_PARENTHESIS -> ()
+    | Error e ->
+        assert_failure ("Incorrectly error for pattern: " ^ show_compile_error e)
+    | Ok _ -> assert_failure "Incorrectly compiled invalid pattern")
+
+let bad_offset ctxt =
+  Interp.(
+    match compile "abc" with
+    | Error e -> assert_failure ("failed to compile: " ^ show_compile_error e)
+    | Ok re ->
+        let printer = [%show: (range option, match_error) result] in
+        assert_equal ~printer (Error BADOFFSET)
+          (find ~subject_offset:(-1) re "ab" >+= range_of_match);
+        assert_equal ~printer (Error BADOFFSET)
+          (find ~subject_offset:10 re "123abc456" >+= range_of_match))
 
 let check_version ctxt =
   let major, minor = Pcre2.version in
@@ -57,6 +80,7 @@ let suite =
          "simple_test" >:: simple_test;
          "simple_test" >:: simple_captures;
          "bad_pattern" >:: bad_pattern;
+         "bad_offset" >:: bad_offset;
          "version" >:: check_version;
        ]
 
